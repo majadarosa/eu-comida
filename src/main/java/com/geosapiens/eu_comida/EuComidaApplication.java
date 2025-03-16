7880568e259a43fea66bfb9088d2512a37c095a5
Maja Sikora <maja@wises.com.br>
2025-03-16 17:35:58 -0300

    chore: configurações iniciais

---
+++ src/main/java/com/geosapiens/eu_comida/EuComidaApplication.java
@@ -0,0 +1,13 @@
+package com.geosapiens.eu_comida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuComidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuComidaApplication.class, args);
+	}
+
+}
